6c410b59871f259df6450d6e8e3f3b4c0496dd60
iLLviA <illvia.3594@gmail.com>
2019-10-22 17:44:15 +0900

    fix

--- pages/mypage/settingStage/index.vue
+++ pages/mypage/settingStage/index.vue
@@ -3,17 +3,29 @@
         <h1 class="h3">登壇情報</h1>
         <form action="" class="mb-5">
             <div class="form-group">
-                <input type="text" class="form-control" placeholder="タイトル">
+                <input type="text" 
+                    v-model="$v.form.title.$model"
+                    class="form-control"  
+                    placeholder="タイトル"
+                    @change="inputForm('title')" >
             </div>
+            <p class="p-error" v-if="!$v.form.title.required && $v.form.title.$dirty">タイトルは必須です</p>
             <div class="form-group">
-                <textarea class="form-control" rows="5" placeholder="登壇情報入力"></textarea>
+                <textarea 
+                    class="form-control" 
+                    v-model="$v.form.info.$model"
+                    rows="5" 
+                    placeholder="登壇情報入力"
+                    @change="inputForm('info')">
+                </textarea>
             </div>
+            <p class="p-error" v-if="!$v.form.info.required && $v.form.info.$dirty">タイトルは必須です</p>
         </form>
         <div class="pl-3 mb-5">
             <h2 class="h3">イベント情報</h2>
             <p>2019/10/01   chatbox 勉強会       ↓ 大阪</p>
         </div>
-        <div class="b-5 pl-2">
+        <div class="mb-5 pl-2">
             <h2 class="h3">登壇者情報</h2>
             <div class="row position-relative">
                 <div class="">
@@ -26,7 +38,9 @@
                 <nuxt-link to="/mypage" class="p-link">戻る</nuxt-link>
             </div>
         </div>
-        
+        <div class="text-center">
+            <button class="btn btn-primary text-center" @click="addInfo">追加する</button>
+        </div>
     </section>
 </template>
 
@@ -34,13 +48,43 @@
 import firebase from '~/plugins/firebase'
 import auth from '~/plugins/auth'
 import userMapper from '@/store/user'
+import validations from '@/service/validations/stageInfo'
 
 export default {
+    data(){
+        return {
+            form: {
+                title: null,
+                info: null
+            }
+        }
+    },
     computed: {
         ...userMapper.mapGetters(['user'])
     },
     methods: {
-        ...userMapper.mapMutations(['initUser', 'setUser'])
+        ...userMapper.mapMutations(['initUser', 'setUser']),
+        inputForm(name){
+            if(name === 'title'){
+                this.$v.form.title.$touch()
+                if(this.$v.form.title.$invalid){
+                    return ;
+                }
+            }
+            if(name === 'info'){
+                this.$v.form.info.$touch()
+                if(this.$v.form.info.$invalid){
+                    return ;
+                }
+            }
+        },
+        addInfo(){
+            this.$v.form.$touch()
+            if(this.$v.form.$invalid) {
+                console.error("エラー内容を確認してください。")
+                return ;
+            }
+        }
     },  
     async mounted() {
         const user = await auth()
@@ -49,7 +93,8 @@ export default {
         }else {
             this.$router.push('/login')
         }
-    }
+    },
+    validations
 }
 </script>
 
@@ -64,5 +109,8 @@ export default {
     position: absolute;
     bottom: 0;
     right: 0;
+}
+.p-error {
+    color: red;
 }
 </style>
